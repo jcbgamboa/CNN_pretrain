016c637cb99cfd43f21df4e1d37c942be7f31d61
John Gamboa <vaulttech@gmail.com>
2016-12-05 15:08:30 +0100

    Fixes script to run CAES.
    For some reason, using parenthesis was not working. Now it is fine.
    
     Changes to be committed:
    	modified:   run_autoencoders.sh

--- run_autoencoders.sh
+++ run_autoencoders.sh
@@ -8,19 +8,21 @@ CIFAR_PATH=./cifar_folds
 CIFAR_FOLDS=6
 CIFAR_OUTPUT_PATH=./pretrained_weights/caes/cifar
 
-mkdir -p $(MNIST_OUTPUT_PATH)
-mkdir -p $(CIFAR_OUTPUT_PATH)
+mkdir -p $MNIST_OUTPUT_PATH
+mkdir -p $CIFAR_OUTPUT_PATH
 
-for ((i = 0; i < $MNIST_FOLDS; i++))
+for ((i = 1; i <= $MNIST_FOLDS; i++))
 do
-	python auto_encoders/caes_nolearn.py $(MNIST_PATH)/fold_$(i).mat \
-			$(MNIST_OUTPUT_PATH)/fold_$(i).mat
+	echo "python auto_encoders/caes_nolearn.py $MNIST_PATH/fold_$i.mat $MNIST_OUTPUT_PATH/fold_$i.mat"
+	python auto_encoders/caes_nolearn.py $MNIST_PATH/fold_$i.mat \
+			$MNIST_OUTPUT_PATH/fold_$i.mat
 done
 
-for ((i = 0; i < $CIFAR_FOLDS; i++))
+for ((i = 1; i <= $CIFAR_FOLDS; i++))
 do
-	python auto_encoders/caes_nolearn.py $(CIFAR_PATH)/fold_$(i).mat \
-			$(CIFAR_OUTPUT_PATH)/fold_$(i).mat
+	echo "python auto_encoders/caes_nolearn.py $CIFAR_PATH/fold_$i.mat $CIFAR_OUTPUT_PATH/fold_$i.mat --normalize"
+	python auto_encoders/caes_nolearn.py $CIFAR_PATH/fold_$i.mat \
+			$CIFAR_OUTPUT_PATH/fold_$i.mat --normalize
 done
 
 
