22c0a06ea42e430ccfc823b0a2fda6d425a2eeb6
John Gamboa <vaulttech@gmail.com>
2016-12-06 00:34:37 +0100

    [minor]: More restructuring of stuff
     Changes to be committed:
    	renamed:    run_networks.m -> networks/run_networks.m
    	renamed:    run_networks_mnist.m -> networks/run_networks_mnist.m
    	renamed:    run_networks_yahoo.m -> networks/run_networks_yahoo.m
    	renamed:    networks/createRollingWindow.m -> networks/utils/data_tools/createRollingWindow.m
    	renamed:    networks/labelmatrix.m -> networks/utils/data_tools/labelmatrix.m
    	modified:   run_autoencoders.sh
    	new file:   yahoo/caes_yahoo.py

--- run_autoencoders.sh
+++ run_autoencoders.sh
@@ -1,12 +1,12 @@
 #! /bin/bash
 
-MNIST_PATH=./mnist_folds
+MNIST_PATH=./datasets/mnist_folds
 MNIST_FOLDS=7
-MNIST_OUTPUT_PATH=./pretrained_weights/caes/mnist
+MNIST_OUTPUT_PATH=./results/pretrained_weights/caes/mnist
 
-CIFAR_PATH=./cifar_folds
+CIFAR_PATH=./datasets/cifar_folds
 CIFAR_FOLDS=6
-CIFAR_OUTPUT_PATH=./pretrained_weights/caes/cifar
+CIFAR_OUTPUT_PATH=./results/pretrained_weights/caes/cifar
 
 mkdir -p $MNIST_OUTPUT_PATH
 mkdir -p $CIFAR_OUTPUT_PATH
